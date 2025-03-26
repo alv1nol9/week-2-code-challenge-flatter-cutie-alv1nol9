2bfc5918eba66edbf6e598a2e2071471569eb15d
alv1nol9 <alvin.kimani@student.moringaschool.com>
2025-03-26 14:15:11 +0300

    fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 // Your code here
+//Your code here
+
+ //fetch & display character names
+ //show character details when clicked
+ //handle votes properly
+ //adding new characters
+ //updating server with votes
+ //>>>fetch
+ 
+ document.addEventListener("DOMContentLoaded", getCharacters)
+ 
+ function getCharacters() {
+     fetch("https://json-chi-six.vercel.app/characters")
+     .then(response=>response.json() )
+     .then( data => {
+        const characterBar= document.getElementById("character-bar")
+        characterBar.innerHTML="";
+    
+        data.forEach(character=>{
+            const span =document.createElement("span");
+            span.textContent = character.name;
+            span.addEventListener("click",()=>showCharacterDetails(character));
+            characterBar.appendChild(span);            
+        })
+     })    
+     .catch(error=>console.log("Characters not fetched",error));    
+ }
+ 
+ 
+ 
+ //>>>>>>done fetching
+ 
+ // display character details
